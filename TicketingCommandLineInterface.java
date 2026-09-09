b2538a14b47db1477712e2bbced0a79becb27a10
fkr-tds <fikertadesse07@gmail.com>
2026-09-09 17:00:11 +0300

    feat: enhance TicketingCommandLineInterface with save functionality and update menu options

--- TicketingCommandLineInterface.java
+++ TicketingCommandLineInterface.java
@@ -17,6 +17,8 @@ public class TicketingCommandLineInterface {
     EventService eventService = new EventService(scan, ticketingRepository);
     ReservationService reservationService = new ReservationService(scan, ticketingRepository);
 
+    ticketingRepository.load();
+
     boolean restartTicketingSystem = true;
 
     do {
@@ -27,8 +29,8 @@ public class TicketingCommandLineInterface {
 
         if (userType == 'O' || userType == 'o') {
             System.out.println("\nYou are logged in as an Operator. What would you like to do?");
-            System.out.println("\n1. Add Venue\t2. List Venues\t3. Add Event\t4. List Events\t5. Add Seats\t6. List Seats");
-            System.out.print("\nPlease select an option (1, 2, 3, 4, 5 or 6): ");
+            System.out.println("\n1. Add Venue\t2. Add Event\t3. Add Seats\t4. List Venues\t5. List Events\t6. List Seats\t7. Exit");
+            System.out.print("\nPlease select an option (1, 2, 3, 4, 5, 6 or 7): ");
 
             if (!scan.hasNextInt()) {
                 System.out.println("\nPlease enter a valid integer.");
@@ -42,32 +44,39 @@ public class TicketingCommandLineInterface {
             switch (operatorChoice) {
                 case 1:
                     venueService.addVenue();
+                    ticketingRepository.save();
                     break;
                 case 2:
                     venueService.listVenues();
+                    eventService.addEvent();
+                    ticketingRepository.save();
                     break;
                 case 3:
                     venueService.listVenues();
-                    eventService.addEvent();
+                    seatService.addSeats();
+                    ticketingRepository.save();
                     break;
                 case 4:
+                    venueService.listVenues();
+                    break;
+
+                case 5:
                     eventService.listEvents();
                     break;
-                case 5:
-                    venueService.listVenues();
-                    seatService.addSeats();
-                    break;
                 case 6:
-                    venueService.listVenues();
                     seatService.listSeats();
+                    break;
+                case 7:
+                    restartTicketingSystem = false;
+                    System.out.println("\nExiting the Ticketing System. Goodbye!");
                     break;
                 default:
                     System.out.println("\nInvalid choice. Please select a valid option.");
             }
         } else if (userType == 'C' || userType == 'c') {
             System.out.println("\nYou are logged in as a Customer. What would you like to do?");
-            System.out.println("\n1. List Venues\t2. List Events\t3. Make Reservation\t4. Confirm Reservation\t5. Cancel Reservation");
-            System.out.print("\nPlease select an option (1, 2, 3. 4 or 5): ");
+            System.out.println("\n1. List Venues\t2. List Events\t3. Make Reservation\t4. Confirm Reservation\t5. Cancel Reservation\t6. Exit");
+            System.out.print("\nPlease select an option (1, 2, 3, 4, 5 or 6): ");
 
             if (!scan.hasNextInt()) {
                 System.out.println("\nPlease enter a valid integer.");
@@ -88,12 +97,20 @@ public class TicketingCommandLineInterface {
                 case 3:
                     eventService.listEvents();
                     reservationService.makeReservation();
+                    ticketingRepository.save();
                     break;
                 case 4:
                     reservationService.confirmReservation();
+                    ticketingRepository.save();
                     break;
                 case 5:
                     reservationService.cancelReservation();
+                    ticketingRepository.save();
+                    break;
+                case 6:
+                    restartTicketingSystem = false;
+                    System.out.println("\nExiting the Ticketing System. Goodbye!");
+                    break;
                 default:
                     System.out.println("\nInvalid choice. Please select a valid option.");
             }
